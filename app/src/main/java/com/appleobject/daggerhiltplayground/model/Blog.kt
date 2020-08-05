557c84c2d90a21d27f8eca2c897a34dd4fcf442f
appleobject <52360868+appleobject@users.noreply.github.com>
2020-08-05 11:45:48 +0100

    initial commit

---
+++ app/src/main/java/com/appleobject/daggerhiltplayground/model/Blog.kt
@@ -0,0 +1,11 @@
+package com.appleobject.daggerhiltplayground.model
+
+data class Blog(
+    var id: Int,
+    var title: String,
+    var body: String,
+    var image: String,
+    var category: String
+) {
+
+}
