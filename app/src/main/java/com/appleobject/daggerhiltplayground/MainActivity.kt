0873899c2b844407d028944bd2040e1ebc24d200
appleobject <52360868+appleobject@users.noreply.github.com>
2020-08-11 22:52:22 +0100

    Type: Feat
    Add a RoomModule.
    
    With a function that return the Room Database builder.

--- app/src/main/java/com/appleobject/daggerhiltplayground/MainActivity.kt
+++ app/src/main/java/com/appleobject/daggerhiltplayground/MainActivity.kt
@@ -2,8 +2,14 @@ package com.appleobject.daggerhiltplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "AppDebug"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/appleobject/daggerhiltplayground/di/RoomModule.kt
@@ -0,0 +1,35 @@
+package com.appleobject.daggerhiltplayground.di
+
+import android.content.Context
+import androidx.room.Room
+import com.appleobject.daggerhiltplayground.room.BlogDao
+import com.appleobject.daggerhiltplayground.room.BlogDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object RoomModule {
+
+    @Singleton
+    @Provides
+    fun provideBlogDb(@ApplicationContext context: Context): BlogDatabase{
+        return Room.databaseBuilder(
+            context,
+            BlogDatabase::class.java,
+            BlogDatabase.DATABASE_NAME
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideBlogDAO(blogDatabase: BlogDatabase): BlogDao {
+        return blogDatabase.BlogDao()
+    }
+}
